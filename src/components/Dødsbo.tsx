ce8acff45616ed70fe0cc99112d5c3fdca5fbb73
Jens <jenspoo@stud.ntnu>
2021-02-12 14:03:09 +0100

    La til epost utfylling (#6)

--- src/components/Dødsbo.tsx
+++ src/components/Dødsbo.tsx
@@ -7,9 +7,9 @@ import {  Modal,
           CssBaseline } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Alert from '@material-ui/lab/Alert';
-
-
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -17,7 +17,10 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "20px",
     width:"500px",
     backgroundColor: "#F5F5F5",
-    borderRadius: 5
+    borderRadius: 5,
+    maxHeight: "calc(100vh - 40px)",
+    overflow: "auto",
+    position: "relative"
   },
   removeOutline: {
     outline: 0
@@ -25,10 +28,10 @@ const useStyles = makeStyles((theme) => ({
   removeBorderRadius: {
     borderRadius: 0,
   },
-  button: {
+  submitButton: {
     borderTopRightRadius: 0,
     borderTopLeftRadius: 0,
-    padding: 14,
+    padding: 14
   },
   TextField: {
     marginLeft: 0,
@@ -41,6 +44,12 @@ const useStyles = makeStyles((theme) => ({
   title: {
     padding: 10,
     boxShadow: "0px 4px 5px -5px",
+  },
+  displayInlineBlock: {
+    display: "inline-block"
+  },
+  emailButton: {
+    padding: 5
   }
 }));
 
@@ -50,40 +59,24 @@ const Dødsbo: React.FC<any> = (props) => {
     const classes = useStyles();
     const [name, setName] = useState("");
     const [description, setDescription] = useState(""); 
-    const [members, setMembers] = useState([]);
-    const [objects, setObjects] = useState([]);
-    const [validInput, setValidInput] = useState(true);
+    const [members, setMembers] = useState(new Array<string>());
 
     function createNewDødsbo(name: string, description: string, members: [], objects: []) {
         setName(name)
         setDescription(description)
         setMembers(members)
-        setObjects(objects)
     }
 
     const resetState = () => {
       setName("");
       setDescription("");
       setMembers([]);
-      setObjects([]);
-    }
-
-    const checkFormData = () => {
-      setValidInput((name && description) != "");
-    }
-
-    const renderAlert = () => {
-      if (!validInput) return (
-        <Alert variant="filled" severity="warning" className={classes.removeBorderRadius}>
-          Vennligst fyll inn alle felt merket med (*)
-        </Alert>);
     }
 
     return (
       <Modal
         open={props.openModal}
         onClose = { () => {
-          setValidInput(true);
           props.closeModal();
           resetState();
         }}
@@ -91,52 +84,94 @@ const Dødsbo: React.FC<any> = (props) => {
 
         {
           <Container className={`${classes.removeOutline} ${classes.container}`} maxWidth="sm" disableGutters>
+          <form>
             <div className={classes.title}>
               <Typography align="center" component="h1" variant="h5">
                 Opprett et dødsbo
               </Typography>
             </div>
+            <IconButton 
+                style={{margin: "10px", padding: 5, position: "absolute", top: 0, right: 0}} 
+                
+                color="primary" 
+                edge="end" 
+                aria-label="close"
+                onClick={(e) => {
+                  props.closeModal();
+                  resetState();
+                }}
+              >
+              <CloseIcon />
+            </IconButton>
             <CssBaseline />
             <div className={classes.textFieldWrapper}>
               <TextField
                 id="standard-full-width"
                 className={classes.TextField}
-                placeholder="Navn på dødsbo*"
+                label="Navn på dødsbo"
                 fullWidth
+                required
                 margin="normal"
-                InputLabelProps={{
-                  shrink: true,
-                }}
                 onChange={(e) => {setName(e.target.value)}}
               />
               <TextField
                 id="standard-full-width"
                 className={classes.TextField}
-                placeholder="Beskrivelse"
+                label="Beskrivelse"
                 fullWidth
                 margin="normal"
                 variant="filled"
                 multiline
                 rows={3}
-                InputLabelProps={{
-                  shrink: true,
-                }}
                 onChange={(e) => {setDescription(e.target.value)}}
               />
-            </div>
-            {renderAlert()}
-            <Button
-                className={classes.button}
-                fullWidth
-                variant="contained"
-                color="primary"
-                onClick={() => {
-                    checkFormData();
-                    if (validInput) props.getFormData({name: name, description: description});
+              <IconButton 
+                style={{marginRight: 0, padding: 5}} 
+                className={classes.displayInlineBlock} 
+                color="primary" 
+                edge="end" 
+                aria-label="add"
+                onClick={(e) => {
+                  setMembers(members.concat(""))
                 }}
+              >
+                <AddIcon />
+              </IconButton>
+              <Typography component="h5" variant="subtitle1" className={classes.displayInlineBlock}>
+                Legg til medlem
+              </Typography>
+              {members.map((item, i) => <TextField
+                key={i}
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                type="email"
+                onChange={(e) => {
+                  members[i] = e.target.value;
+                  setMembers(members);
+                }}
+              />)}
+            </div>
+            <Button
+              className={classes.submitButton}
+              fullWidth
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                props.getFormData({
+                  name: name, 
+                  description: description, 
+                  members: members.filter(member => member != "")
+                });
+              }}
             >
-                Oprett Nytt Dødsbo
+              Oprett Nytt Dødsbo
             </Button>
+          </form>
           </Container>
         }
       </Modal>
