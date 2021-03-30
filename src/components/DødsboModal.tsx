3a6ec8d2335b9e57a7205eb25318bf5269bf4f81
Jens Petter Oostendorp <jenspoo@stud.ntnu.no>
2021-03-30 13:10:36 +0200

    removed unused styles in DødsboModal (#44)

--- src/components/DødsboModal.tsx
+++ src/components/DødsboModal.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import {
-  Modal,
   Button,
   CssBaseline,
   Typography,
-  Container,
   IconButton,
   makeStyles,
   TextField,
@@ -15,49 +13,17 @@ import {
   DialogActions,
 } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
-import CloseIcon from "@material-ui/icons/Close";
 import Service from "../services/Service";
 import { v4 as uuidv4 } from "uuid";
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    margin: "auto",
-    marginTop: "20px",
-    width: "500px",
-    backgroundColor: "#F5F5F5",
-    borderRadius: 5,
-    maxHeight: "calc(100vh - 40px)",
-    overflow: "auto",
-    position: "relative",
-  },
-  removeOutline: {
-    outline: 0,
-  },
-  removeBorderRadius: {
-    borderRadius: 0,
-  },
-  submitButton: {
-    borderTopRightRadius: 0,
-    borderTopLeftRadius: 0,
-    padding: 14,
-  },
   TextField: {
     marginLeft: 0,
     marginRight: 0,
     margin: 8,
   },
-  textFieldWrapper: {
-    padding: "16px",
-  },
-  title: {
-    padding: 10,
-    boxShadow: "0px 4px 5px -5px",
-  },
   displayInlineBlock: {
     display: "inline-block",
-  },
-  emailButton: {
-    padding: 5,
   },
 }));
 
